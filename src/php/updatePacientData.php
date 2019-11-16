c9e14ca77578687b5662c89d0906006b61f570fe
gerardo rodriguez <theronin115@gmail.com>
2019-11-16 01:09:09 -0600

    correcion de un bug relacionado con los metaphonos y updatePacientData.php que causaba que cuando se cambiaba el nombre de un paciente este no pudiera ser encontrado por su nuevo nombre

--- src/php/updatePacientData.php
+++ src/php/updatePacientData.php
@@ -3,6 +3,7 @@
     $new_value = filter_var($_POST["new_value"],FILTER_SANITIZE_STRING);
     $uuid = filter_var($_POST["uuid"],FILTER_SANITIZE_STRING);
     include "connect.php";
+    include "nameSound.php";
 
     $sql = "UPDATE pacientes SET $attrib=? WHERE uuid=?";
     
@@ -13,6 +14,10 @@
         $value_types =  ($_POST["value_type"]==="string" ? "s" : "d")."s";
         mysqli_stmt_bind_param($stmt,$value_types,$new_value,$uuid);
         mysqli_stmt_execute($stmt);
+        if($attrib === "name")
+        {
+            updateMetaphones($conn,$uuid,$new_value);
+        }
     }
     $conn->close();
     updateOnJson($uuid,$attrib,$new_value);
@@ -32,6 +37,19 @@
                 $json[$attrib] = $new_value;
                 file_put_contents($path.DIRECTORY_SEPARATOR.$item,json_encode($json));
             }
+        }
+    }
+
+    function updateMetaphones($conn,$uuid,$name)
+    {
+        $sql = "UPDATE pacientes SET f_sound=?, s_sound=?, t_sound=? WHERE uuid=? LIMIT 1";
+
+        $sound = getNameSound($name);
+        $stmt = $conn->stmt_init();
+        if(mysqli_stmt_prepare($stmt,$sql))
+        {
+            mysqli_stmt_bind_param($stmt,'ssss',$sound[0],$sound[1],$sound[2],$uuid);
+            mysqli_stmt_execute($stmt);
         }
     }
 ?>
